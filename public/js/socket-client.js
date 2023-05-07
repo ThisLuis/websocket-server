6e952168911697030b2add5852134aaab6dd34d0
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-05-06 22:45:39 -0600

    Configuramos la deteccion de conexion y desconexion de un cliente al servidor

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,4 +1,19 @@
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
 
 // Este es el socket del cliente que esta usando la app
 // El io() es el que importamos en el html
 const socket = io();
+
+// Listeners
+socket.on('connect', () => {
+    console.log('Conectado');
+    lblOnline.style.display = ''
+    lblOffline.style.display = 'none';
+});
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+    lblOnline.style.display = 'none'
+    lblOffline.style.display = '';
+});
